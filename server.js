f55004c2d2a833bd0863dfcd66fa3131b3d4d128
Grisha Egorov <egorovg13@gmail.com>
2020-12-24 16:32:52 +0300

    prototype complete

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+
+app.use(express.static(path.join(process.cwd(), 'public')));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+});
+
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
